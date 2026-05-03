028ab3c0ab2a926b9a9f3614ff75d8ef60a10996
ttbas73-bit <ttbas73@gmail.com>
2026-05-04 00:03:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ async function loadData() {
 
 window.addEventListener('load', () => {
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('../sw.js');
+        navigator.serviceWorker.register('./sw.js');
     }
     checkInstallStatus();
     onAuthStateChanged(auth, (user) => {
